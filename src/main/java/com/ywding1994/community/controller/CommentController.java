57a67afd0c0f9585ec421559a4f0c1123b677336
ywding1994 <ywding1994@126.com>
2022-08-03 01:20:25 +0800

    1. 评论接口新增消息通知.

--- src/main/java/com/ywding1994/community/controller/CommentController.java
+++ src/main/java/com/ywding1994/community/controller/CommentController.java
@@ -1,5 +1,7 @@
 package com.ywding1994.community.controller;
 
+import java.util.Map;
+
 import javax.annotation.Resource;
 
 import org.springframework.stereotype.Controller;
@@ -8,8 +10,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.ywding1994.community.constant.CommentConstant;
+import com.ywding1994.community.constant.CommunityConstant;
 import com.ywding1994.community.entity.Comment;
+import com.ywding1994.community.entity.DiscussPost;
+import com.ywding1994.community.event.Event;
+import com.ywding1994.community.event.EventProducer;
 import com.ywding1994.community.service.CommentService;
+import com.ywding1994.community.service.DiscussPostService;
 import com.ywding1994.community.util.HostHolder;
 
 import io.swagger.annotations.Api;
@@ -27,12 +34,46 @@ public class CommentController {
     @Resource
     private HostHolder hostHolder;
 
+    @Resource
+    private EventProducer eventProducer;
+
+    @Resource
+    private DiscussPostService discussPostService;
+
     @RequestMapping(path = "/add/{discussPostId}", method = RequestMethod.POST)
     @ApiOperation(value = "发布评论", httpMethod = "POST")
     public String addComment(@PathVariable("discussPostId") @ApiParam("讨论帖id") int discussPostId, Comment comment) {
         comment.setUserId(hostHolder.getUser().getId());
         comment.setStatus(CommentConstant.Status.NORMAL);
         commentService.addComment(comment);
+
+        // 触发评论事件
+        Event event = Event.builder()
+                .topic(CommunityConstant.TOPIC_COMMENT)
+                .userId(hostHolder.getUser().getId())
+                .entityType(comment.getEntityType())
+                .entityId(comment.getEntityId())
+                .data(Map.of("postId", discussPostId))
+                .build();
+        if (comment.getEntityType() == CommunityConstant.ENTITY_TYPE_POST) {
+            DiscussPost target = discussPostService.getById(comment.getEntityId());
+            event.setEntityUserId(target.getUserId());
+        } else if (comment.getEntityType() == CommunityConstant.ENTITY_TYPE_COMMENT) {
+            Comment target = commentService.getById(comment.getEntityId());
+            event.setEntityUserId(target.getUserId());
+        }
+        eventProducer.fireEvent(event);
+
+        if (comment.getEntityType() == CommunityConstant.ENTITY_TYPE_POST) {
+            // 触发发帖事件
+            event = Event.builder()
+                    .topic(CommunityConstant.TOPIC_PUBLISH)
+                    .userId(comment.getUserId())
+                    .entityType(CommunityConstant.ENTITY_TYPE_POST)
+                    .build();
+            eventProducer.fireEvent(event);
+        }
+
         return "redirect:/discuss/detail/" + discussPostId;
     }
 
